02b18c721d2b1f026d04a5f36798344c46210710
Kristina Frederick <kristinafrederick@icloud.com>
2021-05-04 23:11:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,10 +30,9 @@ class TicTacToe
     @board[index] = token
   end
 
-  def position_taken?(board, index)
-    board[index] != " " || board[index] != ""
-    board[index] == "X" || board[index] == "O"
-  end
+  def position_taken?(index)
+   @board[index] != " "
+ end
 
   def valid_move?(board, index)
     !position_taken?(board, index) && index.between?(0, 8)
